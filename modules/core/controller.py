f5561c2da832cdd6a4c7fbbe0aef7b9982a2be39
Carl Allen <carl@allenofmn.com>
2017-06-11 22:24:38 -0500

    remove xtra space in cooler_off

--- modules/core/controller.py
+++ modules/core/controller.py
@@ -133,7 +133,6 @@ class FermenterController(ControllerBase, ActorController, SensorController):
 
     @cbpi.try_catch(None)
     def cooler_off(self):
-
         f = self.api.cache.get("fermenter").get(self.fermenter_id)
         if f.cooler is not None:
             self.actor_off(int(f.cooler))
