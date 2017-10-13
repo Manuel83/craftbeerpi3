f07a2bdd7ab548b68ce7f7362a0124c52e68fb73
Manuel83 <manuelfritsch@Manuels-MBP.fritz.box>
2017-10-13 21:54:20 +0200

    Further git test code

--- gittest.py
+++ gittest.py
@@ -1,7 +1,18 @@
+import requests
 from git import Repo, Git
 
 repo = Repo('./')
 print repo
 for remote in repo.remotes:
-    print remote
-    remote.fetch()ls
+
+    remote.fetch()
+
+url = 'https://api.github.com/repos/manuel83/craftbeerpi3/releases'
+response = requests.get(url)
+
+result = []
+
+for r in response.json():
+    result.append({"tag_name": r.get("tag_name"), "timestamp": r.get("created_at")})
+
+print result
