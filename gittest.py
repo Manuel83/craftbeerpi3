ad21625f9bc15a1479579b6b116f019ac6667fee
Manuel83 <manuelfritsch@Manuels-MBP.fritz.box>
2017-10-13 22:36:18 +0200

    update

--- gittest.py
+++ gittest.py
@@ -2,17 +2,26 @@ import requests
 from git import Repo, Git
 
 repo = Repo('./')
-print repo
-for remote in repo.remotes:
+branch = repo.active_branch
 
+print branch.name
+
+for remote in repo.remotes:
     remote.fetch()
 
 url = 'https://api.github.com/repos/manuel83/craftbeerpi3/releases'
 response = requests.get(url)
 
-result = []
+
+result = {"branches":[], "releases": []}
+
+result["branches"].append({"name": "master"})
+
+for branch in repo.branches:
+    result["branches"].append({"name": branch.name})
 
 for r in response.json():
-    result.append({"tag_name": r.get("tag_name"), "timestamp": r.get("created_at")})
+    result["releases"].append({"name": "tags/%s" % r.get("tag_name")})
 
 print result
+
