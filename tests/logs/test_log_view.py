476be946e0ce58ffe3b049f9f8bd7138ee2b0ca8
Johannes <johannes@steibel.de>
2017-10-25 07:47:57 +0200

    changed travis configuration to get py.test to work

--- tests/logs/test_log_view.py
+++ tests/logs/test_log_view.py
@@ -1,19 +1,15 @@
-import unittest
 
 from modules.logs import LogView
 
 
-class TestLogView(unittest.TestCase):
+class TestLogView(object):
 
     def test_get_all_logfiles(self):
+
         LogView._log_directory = "./logs"
 
         log_view = LogView()
         logfiles = log_view.get_all_logfiles()
 
         print(logfiles)
-        self.assertTrue(len(logfiles) > 0)
-
-
-if __name__ == '__main__':
-    unittest.main()
+        assert (len(logfiles) == 2)
