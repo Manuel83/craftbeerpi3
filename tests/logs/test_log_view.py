4d59b9dc0745ee0554c0b1479263aea656508fcb
Johannes <johannes@steibel.de>
2017-10-25 07:24:29 +0200

    changed travis configuration to get py.test to work

--- tests/logs/test_log_view.py
+++ tests/logs/test_log_view.py
@@ -6,7 +6,7 @@ from modules.logs import LogView
 class TestLogView(unittest.TestCase):
 
     def test_get_all_logfiles(self):
-        LogView._log_directory = "../../logs"
+        LogView._log_directory = "./logs"
 
         log_view = LogView()
         logfiles = log_view.get_all_logfiles()
