eb1c5af1a51dec4b8c63653616ebfb022938c29c
Juan Pablo Giménez <jpg@rosario.com>
2020-08-08 17:30:38 -0300

    lint

--- modules/base_plugins/dummy_temp/__init__.py
+++ modules/base_plugins/dummy_temp/__init__.py
@@ -1,31 +1,59 @@
 # -*- coding: utf-8 -*-
+"""
+Dummy sensors
+"""
 import subprocess
 import time
 
 from modules import cbpi, socketio
-from modules.core.hardware import  SensorActive
-from modules import cbpi
+from modules.core.hardware import SensorActive
 from modules.core.props import Property
 
 
 @cbpi.sensor
 class DummyTempSensor(SensorActive):
-
-    temp = Property.Number("Temperature", configurable=True, default_value=5, description="Dummy Temperature as decimal value")
-    inc = Property.Number("Auto increase", configurable=True, default_value=0.5, description="Dummy Temperature increase as decimal value")
-    max_temp = Property.Number("Max temperature", configurable=True, default_value='100', description="Dummy Max. Temperature as decimal value")
-    min_temp = Property.Number("Min temperature", configurable=True, default_value='0', description="Dummy Min. Temperature as decimal value")
+    """
+    Dummy temperature sensor
+    """
+    temp = Property.Number("Temperature",
+                           configurable=True,
+                           default_value=5,
+                           description="Dummy Temperature as decimal value")
+    inc = Property.Number(
+        "Auto increase",
+        configurable=True,
+        default_value=0.5,
+        description="Dummy Temperature increase as decimal value")
+    max_temp = Property.Number(
+        "Max temperature",
+        configurable=True,
+        default_value='100',
+        description="Dummy Max. Temperature as decimal value")
+    min_temp = Property.Number(
+        "Min temperature",
+        configurable=True,
+        default_value='0',
+        description="Dummy Min. Temperature as decimal value")
     current_temp = None
 
     @cbpi.action("Reset")
     def reset(self):
+        """
+        reset to default temp
+        """
         self.current_temp = None
 
     @cbpi.action("Toogle Up/Down")
     def toogle(self):
+        """
+        toogle inc from up/down
+        """
         self.inc *= -1
 
     def stop(self):
+        """
+        stop sensor
+        """
         SensorActive.stop(self)
 
     def execute(self):
